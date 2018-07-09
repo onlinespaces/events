8c1404f2e42656e0ea50ddaad542adde53f9bfb8
Scott Collier <scott@onlinespaces.com>
2018-07-09 12:31:18 +0200

    List should display static data from array

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -3,44 +3,45 @@ import { Button, Item, Icon, List, Segment} from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
-  render() {
-    return (
-      <div>
-        <Segment.Group>
-            <Segment>
-            <Item.Group>
-                <Item>
-                <Item.Image size="tiny" circular src="https://randomuser.me/api/portraits/women/13.jpg" />
-                <Item.Content>
-                    <Item.Header as="a">Event Title</Item.Header>
-                    <Item.Description>
-                    Hosted by <a>hosted by</a>
-                    </Item.Description>
-                </Item.Content>
-                </Item>
-            </Item.Group>
-            </Segment>
-            <Segment>
-            <span>
-                <Icon name="clock" /> date |
-                <Icon name="marker" /> time
-            </span>
-            </Segment>
-            <Segment secondary>
-            <List horizontal>
-                <EventListAttendee/>
-                <EventListAttendee/>
-                <EventListAttendee/>
-            </List>
-            </Segment>
-            <Segment clearing>
-                <span>Description will go here.</span>
-                <Button as="a" color="teal" floated="right" content="View" />
-            </Segment>
-        </Segment.Group>
-      </div>
-    );
-  }
+    render() {
+        const {event} = this.props;
+        return (
+            <div>
+            <Segment.Group>
+                <Segment>
+                <Item.Group>
+                    <Item>
+                    <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+                    <Item.Content>
+                        <Item.Header as="a">{event.title}</Item.Header>
+                        <Item.Description>
+                        Hosted by <a>{event.hostedBy}</a>
+                        </Item.Description>
+                    </Item.Content>
+                    </Item>
+                </Item.Group>
+                </Segment>
+                <Segment>
+                <span>
+                    <Icon name="clock" /> {event.date} |
+                    <Icon name="marker" /> {event.venue}
+                </span>
+                </Segment>
+                <Segment secondary>
+                <List horizontal>
+                    {event.attendees.map((attendee) => (
+                        <EventListAttendee key={attendee.id} attendee={attendee}/>
+                    ))}
+                </List>
+                </Segment>
+                <Segment clearing>
+                    <span>{event.description}</span>
+                    <Button as="a" color="teal" floated="right" content="View" />
+                </Segment>
+            </Segment.Group>
+            </div>
+        );
+    }
 }
 
 export default EventListItem;
