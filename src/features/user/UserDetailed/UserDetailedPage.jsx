9e090581b311dfab6850f58cce6b52336cd246be
Scott Collier <scott@onlinespaces.com>
2018-07-10 10:29:27 +0200

    Create funstional components

---
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserDetailedPage = () => {
+    return (
+        <div>
+            <h1>UserDetailedPage</h1>
+            
+        </div>
+    )
+};
+
+export default UserDetailedPage;
