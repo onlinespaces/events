910c441eeb46daff4c73ba199ec021e85d7ad687
Scott Collier <scott@onlinespaces.com>
2018-07-11 10:28:24 +0200

    SelectInput

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -5,6 +5,8 @@ import cuid from 'cuid';
 import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react';
 import { createEvent, updateEvent } from '../eventActions';
 import TextInput from '../../../app/common/form/TextInput';
+import TextArea from '../../../app/common/form/TextArea';
+import SelectInput from '../../../app/common/form/SelectInput';
 
 const mapState = (state, ownProps) => {
 
@@ -32,6 +34,15 @@ const actions = {
     updateEvent
 };
 
+const category = [
+    {key: 'drinks', text: 'Drinks', value: 'drinks'},
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'film', text: 'Film', value: 'film'},
+    {key: 'food', text: 'Food', value: 'food'},
+    {key: 'music', text: 'Music', value: 'music'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+];
+
 class EventForm extends Component {
     onFormSubmit = (event) => {
         event.preventDefault();
@@ -58,13 +69,35 @@ class EventForm extends Component {
                     <Segment>
                         <Form onSubmit={this.onFormSubmit}>
                             <Header sub color='teal' content='Event Details'/>
-                            <Field name='title' type='text' component={TextInput} placeholder='Give your event a name.'/>
-                            <Field name='category' type='text' component={TextInput} placeholder='What is your event about?'/>
-                            <Field name='description' type='text' component={TextInput} placeholder='Tell about your event.'/>
-                            <Header sub color='teal' content='Event Location Details'/>
-                            <Field name='city' type='text' component={TextInput} placeholder='What city?'/>
-                            <Field name='venue' type='text' component={TextInput} placeholder='What venue?'/>
-                            <Field name='date' type='text' component={TextInput} placeholder='When is your event?'/>
+                            <Field name='title'
+                                   type='text'
+                                   component={TextInput}
+                                   placeholder='Give your event a name.'/>
+                            <Field name='category'
+                                   type='text'
+                                   component={SelectInput}
+                                   options={category}
+                                   placeholder='What is your event about?'/>
+                            <Field name='description'
+                                   type='text'
+                                   component={TextArea}
+                                   placeholder='Tell about your event.'
+                                   rows='3'
+                            />
+                            <Header sub color='teal'
+                                    content='Event Location Details'/>
+                            <Field name='city'
+                                   type='text'
+                                   component={TextInput}
+                                   placeholder='What city?'/>
+                            <Field name='venue'
+                                   type='text'
+                                   component={TextInput}
+                                   placeholder='What venue?'/>
+                            <Field name='date'
+                                   type='text'
+                                   component={TextInput}
+                                   placeholder='When is your event?'/>
                             <Button onClick={this.props.history.goBack} type="button">Cancel</Button>
                             <Button positive type="submit">
                                 Submit
