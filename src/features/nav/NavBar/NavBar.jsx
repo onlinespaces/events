c6f15ef1bef245e386838c34f5735770f6febba9
Scott Collier <scott@onlinespaces.com>
2018-07-12 14:34:39 +0200

    Update profile pic

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -13,7 +13,8 @@ const actions = {
 };
 
 const mapState = (state) => ({
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
 });
 
 class NavBar extends Component {
@@ -32,7 +33,7 @@ class NavBar extends Component {
     };
 
     render() {
-        const {auth} = this.props;
+        const {auth, profile} = this.props;
         const authenticated = auth.isLoaded && !auth.isEmpty;
 
         let authenticatedMenu = '';
@@ -58,7 +59,7 @@ class NavBar extends Component {
                     <Menu.Item as={NavLink} to='/test' name="Test" />
                     {authenticatedMenu}
                     {authenticated ?
-                        <SignedInMenu auth={auth} signOut={this.handleSignOut}/>
+                        <SignedInMenu profile={profile} signOut={this.handleSignOut}/>
                         :
                         <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister}/>}
                 </Container>
