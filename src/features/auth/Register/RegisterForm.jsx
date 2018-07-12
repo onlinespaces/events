ffe26df00c649622f9766510790e76c4c4ea9041
Scott Collier <scott@onlinespaces.com>
2018-07-12 13:41:34 +0200

    Add social login buttons

--- src/features/auth/Register/RegisterForm.jsx
+++ src/features/auth/Register/RegisterForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { combineValidators, isRequired } from 'revalidate';
-import { Form, Segment, Button, Label } from 'semantic-ui-react';
+import { Form, Segment, Button, Label, Divider } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 import TextInput from '../../../app/common/form/TextInput';
 import { registerUser} from "../authActions";
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const actions = {
     registerUser
@@ -43,6 +44,10 @@ const RegisterForm = ({handleSubmit, registerUser, error, invalid, submitting}) 
                     <Button disabled={invalid || submitting} fluid size="large" color="teal">
                         Register
                     </Button>
+                    <Divider horizontal>
+                        Or
+                    </Divider>
+                    <SocialLogin/>
                 </Segment>
             </Form>
         </div>
