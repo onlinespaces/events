80d666184f3d332b909af184b2eebc1337bf07c1
Scott Collier <scott@onlinespaces.com>
2018-07-12 15:58:55 +0200

    Prepare to update user info

---
+++ src/features/user/userActions.jsx
@@ -0,0 +1,19 @@
+import moment from 'moment';
+import { toastr } from 'react-redux-toastr';
+
+export const updateProfile = (user) =>
+    async(dispatch, getState, {getFirebase}) => {
+        const firebase = getFirebase();
+
+        if(user.dateOfBirth) {
+            user.dateOfBirth = moment(user.dateOfBirth).toDate();
+        }
+
+        try {
+            await firebase.updateProfile(user);
+            toastr.success('Success', 'Profile updated.');
+
+        } catch(error) {
+            console.log(error);
+        }
+    };
