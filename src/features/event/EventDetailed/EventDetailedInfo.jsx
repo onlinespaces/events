4229fcf5cc2d3fb7dd63d0057db7c3e9852c8535
Scott Collier <scott@onlinespaces.com>
2018-07-12 10:53:51 +0200

    Format date and times better in events

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import format from 'date-fns/format';
 import { Segment, Grid, Icon, Button } from 'semantic-ui-react';
 import EventDetailedMap from './EventDetailedMap';
 
@@ -33,7 +34,7 @@ class EventDetailedInfo extends Component{
                             <Icon name="calendar" size="large" color="teal"/>
                         </Grid.Column>
                         <Grid.Column width={15}>
-                            <span>{event.date}</span>
+                            <span>{format(event.date, 'dddd Do MMMM')} at{' '} {format(event.date, 'HH:mm')}</span>
                         </Grid.Column>
                     </Grid>
                 </Segment>
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import format from 'date-fns/format';
 import { Button, Item, Icon, List, Segment} from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
@@ -24,7 +25,7 @@ class EventListItem extends Component {
                     </Segment>
                     <Segment>
                     <span>
-                        <Icon name="clock" /> {event.date} |
+                        <Icon name="clock" /> {format(event.date, 'dddd Do MMMM')} at{' '} {format(event.date, 'HH:mm')} |
                         <Icon name="marker" /> {event.venue}
                     </span>
                     </Segment>
