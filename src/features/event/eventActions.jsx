86ffa126bcfba49f472574925c76232a49297212
Scott Collier <scott@onlinespaces.com>
2018-07-12 10:44:33 +0200

    Add toastr notifications

--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,21 +1,36 @@
+import { toastr } from 'react-redux-toastr';
 import {CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT, FETCH_EVENTS} from "./eventConstants";
 import { asyncActionError, asyncActionStart, asyncActionFinish } from "../async/asyncActions";
 import { fetchSampleData} from "../../app/data/mockApi";
 
 export const createEvent = (event) => {
-    return {
-        type: CREATE_EVENT,
-        payload: {
-            event
+    return async dispatch => {
+        try {
+            dispatch({
+                type: CREATE_EVENT,
+                payload: {
+                    event
+                }
+            });
+            toastr.success('Success', 'Event has been created');
+        } catch(error) {
+            toastr.error('Error', 'Something went wrong');
         }
     }
 };
 
 export const updateEvent = (event) => {
-    return {
-        type: UPDATE_EVENT,
-        payload: {
-            event
+    return async dispatch => {
+        try {
+            dispatch({
+                type: UPDATE_EVENT,
+                payload: {
+                    event
+                }
+            });
+            toastr.success('Success', 'Event has been updated');
+        } catch(error) {
+            toastr.error('Error', 'Something went wrong when updating');
         }
     }
 };
