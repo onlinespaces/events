0f16950dece5372227e35b1d2bd446064a9a11d7
Scott Collier <scott@onlinespaces.com>
2018-07-12 14:12:12 +0200

    Facebook Login

--- src/features/auth/SocialLogin/SocialLogin.jsx
+++ src/features/auth/SocialLogin/SocialLogin.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
 
-const SocialLogin = () => {
+const SocialLogin = ({socialLogin}) => {
     return (
         <div>
-            <Button type='button' style={{ marginBottom: '10px'}} fluid color='facebook'>
+            <Button onClick={() => socialLogin('facebook')} type='button' style={{ marginBottom: '10px'}} fluid color='facebook'>
                 <Icon name="facebook" /> Login with Facebook
             </Button>
-            <Button type='button' style={{ marginBottom: '10px'}} fluid color='google plus'>
+            <Button onClick={() => socialLogin('google')} type='button' style={{ marginBottom: '10px'}} fluid color='google plus'>
                 <Icon name="google plus" /> Login with Google
             </Button>
         </div>
