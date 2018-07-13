2ca0e3500ab0d7bb6438859f39794829aff00c95
Scott Collier <scott@onlinespaces.com>
2018-07-13 13:43:05 +0200

    Show main image and list of other images

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import { toastr } from 'react-redux-toastr';
 import {Image, Segment, Header, Divider, Grid, Button, Card, Icon} from 'semantic-ui-react';
 import Cropper from 'react-cropper';
@@ -58,6 +60,15 @@ class PhotosPage extends Component {
     };
 
     render() {
+        const {photos, profile} = this.props;
+        let filteredPhotos;
+
+        if(photos) {
+            filteredPhotos = photos.filter(photo => {
+                return photo.url !== profile.photoURL
+            });
+        }
+
         return (
             <Segment>
                 <Header dividing size='large' content='Your Photos' />
@@ -112,23 +123,47 @@ class PhotosPage extends Component {
 
                 <Card.Group itemsPerRow={5}>
                     <Card>
-                        <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                        <Image src={profile.photoURL}/>
                         <Button positive>Main Photo</Button>
                     </Card>
 
-                    <Card >
-                        <Image
-                            src='https://randomuser.me/api/portraits/men/20.jpg'
-                        />
-                        <div className='ui two buttons'>
-                            <Button basic color='green'>Main</Button>
-                            <Button basic icon='trash' color='red' />
-                        </div>
-                    </Card>
+                    {photos && filteredPhotos.map((photo) =>
+                        <Card key={photo.id}>
+                            <Image
+                                src={photo.url}
+                            />
+                            <div className='ui two buttons'>
+                                <Button basic color='green'>Main</Button>
+                                <Button basic icon='trash' color='red' />
+                            </div>
+                        </Card>
+
+                    )}
+
                 </Card.Group>
             </Segment>
         );
     }
 }
 
-export default connect(null, actions)(PhotosPage);
+const mapState = (state) => ({
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+    photos: state.firestore.ordered.photos
+});
+
+const query = ({auth}) => {
+    return [
+        {
+            collection: 'users',
+            doc: auth.uid,
+            subcollections: [{collection: 'photos'}],
+            storeAs: 'photos'
+        }
+    ]
+};
+
+export default compose (
+    connect(mapState, actions),
+    firestoreConnect(auth => query(auth))
+)(PhotosPage) ;
