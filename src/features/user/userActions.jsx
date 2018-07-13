174cbf412a178c4eb31f8af57d7ba12dc483c802
Scott Collier <scott@onlinespaces.com>
2018-07-13 12:50:25 +0200

    Create upload image action

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -20,3 +20,45 @@ export const updateProfile = (user) =>
             console.log(error);
         }
     };
+
+export const uploadProfileImage = (file, fileName) =>
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        // Is sycronous
+        const user = firebase.auth().currentUser;
+        const path = `${user.id}/user_images`;
+        const options = {
+            name: fileName
+        };
+
+        try {
+            // upload the file to firebase
+            let uploadedFile = await firebase.uploadFile(path, file, null, options);
+            // get url of image
+            let downloadURL = await uploadedFile.uploadTaskSnaphot.downloadURL;
+            let userDoc = await firestore.get(`users/${user.id}`);
+            // Get user doc and see if user already has a photo. If not, then set as main.
+            if(userDoc.data().photoURL) {
+                await firebase.updateProfile({
+                    photoURL: downloadURL
+                });
+
+                await user.updateProfile({
+                    photURL: downloadURL
+                });
+            }
+            // Add new photo as new image in photos collection
+            return await firestore.add({
+                collection: 'users',
+                doc: user.uid,
+                subcollections: [{collection: 'photos'}]
+            }, {
+                name: fileName,
+                url: downloadURL
+            })
+        } catch(error) {
+            console.log(error);
+            throw new Error("Problem uploading photo.");
+        }
+    };
