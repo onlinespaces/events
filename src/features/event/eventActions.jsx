f5125a240559cbf131fd547b475707306ac783cf
Scott Collier <scott@onlinespaces.com>
2018-07-14 16:24:05 +0200

    Create event and association table for users and events

--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,16 +1,22 @@
 import { toastr } from 'react-redux-toastr';
-import {CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT, FETCH_EVENTS} from "./eventConstants";
+import { DELETE_EVENT, UPDATE_EVENT, FETCH_EVENTS} from "./eventConstants";
 import { asyncActionError, asyncActionStart, asyncActionFinish } from "../async/asyncActions";
-import { fetchSampleData} from "../../app/data/mockApi";
+import { fetchSampleData} from '../../app/data/mockApi';
+import { createNewEvent } from '../../app/common/util/helpers';
 
 export const createEvent = (event) => {
-    return async dispatch => {
+    return async (dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        const user = firestore.auth().currentUser;
+        const photoURL = getState().firebase.profile.photoURL;
+        const newEvent = createNewEvent(user, photoURL, event);
         try {
-            dispatch({
-                type: CREATE_EVENT,
-                payload: {
-                    event
-                }
+            let createdEvent = await firestore.add(`events`, newEvent);
+            await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+                eventId: createdEvent.id,
+                userUid: user.uid,
+                eventDate: event.date,
+                host: true
             });
             toastr.success('Success', 'Event has been created');
         } catch(error) {
