5908ae63437c4dc65450eb113f05d4fea9d6218f
Scott Collier <scott@onlinespaces.com>
2018-07-15 12:52:39 +0200

    Add page lazy loading

--- src/features/user/UserDetailed/UserDetailedPhotos.jsx
+++ src/features/user/UserDetailed/UserDetailedPhotos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import {Header, Image, Segment} from "semantic-ui-react";
 
 const UserDetailedPhotos = ({photos}) => {
@@ -7,7 +8,11 @@ const UserDetailedPhotos = ({photos}) => {
             <Header icon='image' content='Photos'/>
                 < Image.Group size='small' >
                     {photos.map((photo) =>
-                        <Image key={photo.id} src={photo.url}/>
+                        <LazyLoad key={photo.id}
+                                  height={150}
+                                  placeholder={<Image src='/assets/user.png'/>}>
+                            <Image src={photo.url}/>
+                        </LazyLoad>
                     )}
                 </Image.Group>
         </Segment>
