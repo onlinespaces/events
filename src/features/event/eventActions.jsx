fdf9f9f6e9b9bb86db73f7496decde4800cab96a
Scott Collier <scott@onlinespaces.com>
2018-08-28 15:55:16 +0200

    Start filtering

--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -60,23 +60,44 @@ export const cancelToggle = (cancelled, eventId) =>
         }
     };
 
-export const getEventsForDashboard = () =>
+export const getEventsForDashboard = (lastEvent) =>
     async (dispatch, getState) => {
         let today = new Date(Date.now());
         const firestore = firebase.firestore();
-        const eventsQuery = firestore.collection('events').where('date', '>=', today);
+        const eventsRef= firestore.collection('events');
 
         try {
             dispatch(asyncActionStart());
-            let querySnap = await eventsQuery.get();
+            let startAfter = lastEvent && await firestore.collection('events').doc(lastEvent.id).get();
+            let query;
+            
+            lastEvent ? query = eventsRef
+                    .where('date', '>=', today)
+                    .orderBy('date')
+                    .startAfter(startAfter)
+                    .limit(2)
+                : query = eventsRef
+                    .where('date', '>=', today)
+                    .orderBy('date')
+                    .limit(2);
+
+            let querySnap = await query.get();
+
+            if(querySnap.docs.length === 0) {
+                dispatch(asyncActionFinish());
+                return;
+            }
+
             let events = [];
 
             for(let i = 0; i < querySnap.docs.length; i++) {
                 let evt = {...querySnap.docs[i].data(), id: querySnap.docs[i].id};
                 events.push(evt);
             }
+
             dispatch({type: FETCH_EVENTS, payload: {events}});
             dispatch(asyncActionFinish());
+            return querySnap;
         } catch(error) {
             console.log(error);
             dispatch(asyncActionError());
