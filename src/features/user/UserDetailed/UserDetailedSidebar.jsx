be084554d98e673c32f84e6afac0d876968ae84b
Scott Collier <scott@onlinespaces.com>
2018-08-30 15:59:50 +0200

    Should be able to foloow and unfollow users

--- src/features/user/UserDetailed/UserDetailedSidebar.jsx
+++ src/features/user/UserDetailed/UserDetailedSidebar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import {Button, Segment} from "semantic-ui-react";
+import { Button, Grid, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const UserDetailedSidebar = ({isCurrentUser}) => {
+const UserDetailedSidebar = ({ isCurrentUser, followUser, profile, isFollowing, unfollowUser }) => {
     return (
-        <Segment>
-            {isCurrentUser ?
-                <Button as={Link} to='/settings' color='teal' fluid basic content='Edit Profile'/>
-                :
-                <Button color='teal' fluid basic content='Follow User'/>
-            }
-        </Segment>
-    )};
+        <Grid.Column width={4}>
+            <Segment>
+                {isCurrentUser &&
+                <Button as={Link} to="/settings" color="teal" fluid basic content="Edit Profile" />}
+                {!isCurrentUser &&
+                    !isFollowing &&
+                <Button onClick={() => followUser(profile)} color="teal" fluid basic content="Follow user" />}
+
+                {!isCurrentUser && isFollowing &&
+                <Button onClick={() => unfollowUser(profile)} color="teal" fluid basic content="Unfollow" />}
+            </Segment>
+        </Grid.Column>
+    );
+};
 
 export default UserDetailedSidebar;
